1a1f0b594c21267aa2b84505aa5900b4b0113839
Uthappa <gayanuthappams@gmail.com>
2024-11-15 22:42:09 -0500

    Modified the commenting for the files

--- test/integration_test_node.cpp
+++ test/integration_test_node.cpp
@@ -1,26 +1,13 @@
-// Copyright 2023 Nick Morales.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// # CHANGES:
-//
-// 2024-10-31, Tommy Chang
-//     - Renamed the first test case to "service_test".
-//     - Added a second test, "talker_test".
-//     - Rewrite the first test case to use wait_for_service().
-//     - Use modern ROS2 syntax
-//     - Use Catch2 Fixture
-
+/**
+ * @file publisher_member_function.cpp
+ * @author Uthappa Madettira (120305085)
+ * @brief
+ * @version 0.1
+ * @date 2024-11-06
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 #include <beginner_tutorials/srv/change_str.hpp>
 #include <catch_ros2/catch_ros2.hpp>
 #include <chrono>
